dbbfc0eb1ce0f764e93c9beac321d20101ec689b
LeonVreling <l.m.vreling@student.tue.nl>
2021-03-20 12:00:46 +0100

    fix(board): fix board creation tests

--- test/scripts/mock.js
+++ test/scripts/mock.js
@@ -2,6 +2,7 @@ const nock = require('nock');
 const path = require('path');
 
 const config = require('../../config');
+const body = require('../assets/core-local.json').data;
 
 exports.cleanAll = () => nock.cleanAll();
 
