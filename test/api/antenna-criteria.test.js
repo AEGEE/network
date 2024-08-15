d70b2a2f62079da97660cec369e2e43d87216d54
Leon Vreling <l.m.vreling@student.tue.nl>
2024-08-15 17:08:15 +0200

    feat(network): Add tests for Antenna Criteria
    TODO: Permissions

---
+++ test/api/antenna-criteria.test.js
@@ -0,0 +1,259 @@
+const { startServer, stopServer } = require('../../lib/server');
+const { request } = require('../scripts/helpers');
+const mock = require('../scripts/mock');
+const generator = require('../scripts/generator');
+
+describe('Antenna Criteria', () => {
+    beforeAll(async () => {
+        await startServer();
+    });
+
+    afterAll(async () => {
+        await stopServer();
+    });
+
+    beforeEach(async () => {
+        mock.mockAll();
+    });
+
+    afterEach(async () => {
+        mock.cleanAll();
+    });
+
+    // test('Should fail listing Antenna Criteria if no permission', async () => {
+    //     mock.mockAll({ mainPermissions: { noPermissions: true } });
+
+    //     const res = await request({
+    //         uri: '/antennaCriteria/1',
+    //         method: 'GET',
+    //         headers: { 'X-Auth-Token': 'blablabla' }
+    //     });
+
+    //     expect(res.statusCode).toEqual(403);
+    //     expect(res.body.success).toEqual(false);
+    // });
+
+    test('Should list all Antenna Criteria of the selected Agora', async () => {
+        await generator.createAntennaCriterion({ agora_id: 1 });
+        await generator.createAntennaCriterion({ agora_id: 1 });
+
+        const res = await request({
+            uri: '/antennaCriteria/1',
+            method: 'GET',
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).toHaveProperty('data');
+        expect(res.body.data.length).toEqual(2);
+    });
+
+    test('Should only list Antenna Criteria of the selected Agora', async () => {
+        await generator.createAntennaCriterion({ agora_id: 1 });
+        await generator.createAntennaCriterion({ agora_id: 1 });
+        await generator.createAntennaCriterion({ agora_id: 2 });
+
+        const res = await request({
+            uri: '/antennaCriteria/1',
+            method: 'GET',
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).toHaveProperty('data');
+        expect(res.body.data.length).toEqual(2);
+    });
+
+    // test('Should fail creating new Antenna Criterion if no permission', async () => {
+    //     mock.mockAll({ mainPermissions: { noPermissions: true } });
+
+    //     const criterion = generator.generateAntennaCriterion();
+
+    //     const res = await request({
+    //         uri: '/antennaCriteria',
+    //         method: 'PUT',
+    //         body: criterion,
+    //         headers: { 'X-Auth-Token': 'blablabla' }
+    //     });
+
+    //     expect(res.statusCode).toEqual(403);
+    //     expect(res.body.success).toEqual(false);
+    // });
+
+    test('Should fail creating new Antenna Criterion if agora_id is not set', async () => {
+        const criterion = generator.generateAntennaCriterion({ agora_id: null });
+
+        const res = await request({
+            uri: '/antennaCriteria',
+            method: 'PUT',
+            body: criterion,
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(422);
+        expect(res.body.succes).toEqual(false);
+        expect(res.body).toHaveProperty('errors');
+        expect(res.body).not.toHaveProperty('data');
+        expect(res.body.errors).toHaveProperty('agora_id');
+    });
+
+    test('Should fail creating new Antenna Criterion if body_id is not set', async () => {
+        const criterion = generator.generateAntennaCriterion({ body_id: null });
+
+        const res = await request({
+            uri: '/antennaCriteria',
+            method: 'PUT',
+            body: criterion,
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(422);
+        expect(res.body.succes).toEqual(false);
+        expect(res.body).toHaveProperty('errors');
+        expect(res.body).not.toHaveProperty('data');
+        expect(res.body.errors).toHaveProperty('body_id');
+    });
+
+    test('Should fail creating new Antenna Criterion if antenna_criterion is not set', async () => {
+        const criterion = generator.generateAntennaCriterion({ antenna_criterion: null });
+
+        const res = await request({
+            uri: '/antennaCriteria',
+            method: 'PUT',
+            body: criterion,
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(422);
+        expect(res.body.succes).toEqual(false);
+        expect(res.body).toHaveProperty('errors');
+        expect(res.body).not.toHaveProperty('data');
+        expect(res.body.errors).toHaveProperty('antenna_criterion');
+    });
+
+    test('Should fail creating new Antenna Criterion if antenna_criterion is not correct', async () => {
+        const criterion = generator.generateAntennaCriterion({ antenna_criterion: 'blabla' });
+
+        const res = await request({
+            uri: '/antennaCriteria',
+            method: 'PUT',
+            body: criterion,
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(422);
+        expect(res.body.succes).toEqual(false);
+        expect(res.body).toHaveProperty('errors');
+        expect(res.body).not.toHaveProperty('data');
+        expect(res.body.errors).toHaveProperty('antenna_criterion');
+    });
+
+    test('Should fail creating new Antenna Criterion if value is not correct', async () => {
+        const criterion = generator.generateAntennaCriterion({ value: 'blabla' });
+
+        const res = await request({
+            uri: '/antennaCriteria',
+            method: 'PUT',
+            body: criterion,
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(422);
+        expect(res.body.succes).toEqual(false);
+        expect(res.body).toHaveProperty('errors');
+        expect(res.body).not.toHaveProperty('data');
+        expect(res.body.errors).toHaveProperty('antenna_criterion');
+    });
+
+    test('Should create new Antenna Criterion if everything is okay', async () => {
+        const criterion = generator.generateAntennaCriterion();
+
+        const res = await request({
+            uri: '/antennaCriteria',
+            method: 'PUT',
+            body: criterion,
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.succes).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+        expect(res.body.data).toHaveProperty('agora_id');
+        expect(res.body.data).toHaveProperty('body_id');
+        expect(res.body.data).toHaveProperty('antenna_criterion');
+        expect(res.body.data).not.toHaveProperty('value');
+        expect(res.body.data).not.toHaveProperty('comment');
+    });
+
+    test('Should create new Antenna Criterion with value if everything is okay', async () => {
+        const criterion = generator.generateAntennaCriterion({ value: 'exception' });
+
+        const res = await request({
+            uri: '/antennaCriteria',
+            method: 'PUT',
+            body: criterion,
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.succes).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+        expect(res.body.data).toHaveProperty('agora_id');
+        expect(res.body.data).toHaveProperty('body_id');
+        expect(res.body.data).toHaveProperty('antenna_criterion');
+        expect(res.body.data).toHaveProperty('value');
+        expect(res.body.data).not.toHaveProperty('comment');
+    });
+
+    test('Should create new Antenna Criterion with comment if everything is okay', async () => {
+        const criterion = generator.generateAntennaCriterion({ comment: 'blabla' });
+
+        const res = await request({
+            uri: '/antennaCriteria',
+            method: 'PUT',
+            body: criterion,
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.succes).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+        expect(res.body.data).toHaveProperty('agora_id');
+        expect(res.body.data).toHaveProperty('body_id');
+        expect(res.body.data).toHaveProperty('antenna_criterion');
+        expect(res.body.data).not.toHaveProperty('value');
+        expect(res.body.data).toHaveProperty('comment');
+    });
+
+    test('Should update Antenna Criterion if everything is okay', async () => {
+        await generator.createAntennaCriterion({ agora_id: 1, body_id: 2, antenna_criterion: 'communication', value: 'no', comment: 'No communication' });
+        const criterion = generator.generateAntennaCriterion({ agora_id: 1, body_id: 2, antenna_criterion: 'communication', value: 'yes', comment: 'They are responding!' });
+
+        const res = await request({
+            uri: '/antennaCriteria',
+            method: 'PUT',
+            body: criterion,
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.succes).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+        expect(res.body.data).toHaveProperty('agora_id');
+        expect(res.body.data.agora_id).toEqual(1);
+        expect(res.body.data).toHaveProperty('body_id');
+        expect(res.body.data.body_id).toEqual(2)
+        expect(res.body.data).toHaveProperty('antenna_criterion');
+        expect(res.body.data.antenna_criterion).toEqual('communication');
+        expect(res.body.data).toHaveProperty('value');
+        expect(res.body.data.value).toEqual('yes');
+        expect(res.body.data).toHaveProperty('comment');
+        expect(res.body.data.comment).toEqual('They are responding!');
+    });
+});
